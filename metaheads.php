f1d7198a21b552fb12596cc0d2233fbfb97b0bc2
Showole Oladayo <show4ril@gmail.com>
2023-08-30 22:49:40 +0100

    changes

--- metaheads.php
+++ metaheads.php
@@ -5,9 +5,7 @@
     <title>#1 digital agency in ibadan | We build websites and Mobiles Apps Hamzeal Digital</title>
     <meta content="#1 Digital Agency that build Websites for Oragnizations and Individuals" name="description" />
 
-    <meta
-        content="Mobile Application Development | Ecommerce Webistes | Web Development | Digital Marketing | Hosting | Graphics Design"
-        name="keywords" />
+    <meta content="Mobile Application Development | Ecommerce Webistes | Web Development | Digital Marketing | Hosting | Graphics Design" name="keywords" />
 
 
     <meta name="description" content="Hamzeal Digital | Web Designer | Training | Mobile App development" />
@@ -15,8 +13,7 @@
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="Website Designer in Ibadan / websites / mobile App Dev / Graphics Designer / " />
-    <meta property="og:description"
-        content="Hamzeal Digital&amp; offers top quality ,websites  delivering quality. Ibadan" />
+    <meta property="og:description" content="Hamzeal Digital&amp; offers top quality ,websites  delivering quality. Ibadan" />
     <meta property="og:url" content="https://hamzealdigital.com/" />
     <meta property="og:site_name" content="Hamzeal Digital" />
     <meta property="article:publisher" content="https://www.facebook.com/hamzealdigital/" />
@@ -32,9 +29,7 @@
     <!-- Google Fonts -->
     <!-- <link href="./assets/css/google-font2.css" rel="stylesheet" /> -->
 
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 
@@ -49,55 +44,55 @@
     <link href="assets/css/style.css" rel="stylesheet" />
 
     <style>
-    .float-whatsapp {
-        position: fixed;
-        width: 60px;
-        height: 60px;
-        bottom: 40px;
-        right: 40px;
-        background-color: #25d366;
-        color: #FFF;
-        border-radius: 50px;
-        text-align: center;
-        font-size: 30px;
-        box-shadow: 2px 2px 3px #999;
-        z-index: 100;
-    }
+        .float-whatsapp {
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 40px;
+            background-color: #25d366;
+            color: #FFF;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+        }
 
-    .my-whatsapp-float {
-        margin-top: 16px;
-    }
+        .my-whatsapp-float {
+            margin-top: 16px;
+        }
 
-    .load-more-button {
+        .load-more-button {
 
-        color: #0E1B4D;
-        border: none;
-        padding: 10px 20px;
-        cursor: pointer;
-        border-radius: 4px;
-        font-size: 16px;
-        margin-top: 10px;
-    }
+            color: #0E1B4D;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            border-radius: 4px;
+            font-size: 16px;
+            margin-top: 10px;
+        }
     </style>
     <!-- Google Tag Manager -->
     <script>
-    (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-557JZVST');
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-557JZVST');
     </script>
     <!-- End Google Tag Manager -->
-    <script src="./assets/js/react-dev.js" crossorigin></script>
+    <!-- <script src="./assets/js/react-dev.js" crossorigin></script>
     <script src="./assets/js/react-dom.js" crossorigin></script>
-    <script src="./assets/js/babel.js"></script>
+    <script src="./assets/js/babel.js"></script> -->
 </head>
