d66846e8f44b740f24660931ee630640eaa16679
Showole Oladayo <show4ril@gmail.com>
2023-08-30 22:56:43 +0100

    changes

--- index.php
+++ index.php
@@ -18,27 +18,8 @@
     </script>
 
     <!-- Your SDK code -->
-    <script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            xfbml: true,
-            version: 'v17.0'
-        });
-    };
 
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s);
-        js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-    </script>
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-557JZVST" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
+
     <!-- ======= Header ======= -->
     <?php include 'header.php'; ?>
     <!-- End Header -->
